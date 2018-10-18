416e4b39af5da4eb20e3cf6dd3b88d4af9d5efd9
Ccepero <cindalis.cepero@newbury.edu>
2018-10-17 20:58:38 -0400

    blog post with url update and templates added along with CSS

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ __author__ = 'Cindalis Cepero'
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.BlogListView.as_view(), name= 'home')
-
+    path('', views.BlogListView.as_view(), name= 'home'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
 ]
