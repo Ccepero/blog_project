ef38a3f4011ca47a668baa01f73e34e50de27b1a
Ccepero <cindalis.cepero@newbury.edu>
2018-10-24 19:17:17 -0400

    update post and edit post have been added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from . models import Post
 
 class BlogListView(ListView):
@@ -11,3 +11,13 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
